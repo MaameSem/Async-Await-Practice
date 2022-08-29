f4526c8118fb6dcf5079d93133f53639c200e8b5
Siobhan Vander Pallen <108106935+MaameSem@users.noreply.github.com>
2022-08-29 01:25:25 -0400

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,15 @@
+async function printCityData(cityName) {
+    const res = await fetch(`https://geocode.xyz/${cityName}?json=1&auth=190505517666029746659x121886`);
+    if (res.ok) {
+        const cityData = await res.json();
+        console.log(`lattitude: ${cityData.latt}`);
+        console.log(`longitude: ${cityData.longt}`);
+    } else {
+        const body = await res.text();
+        console.log(`Error: Got status code ${res.status}`);
+        console.log(`Response body: ${body}`);
+    }
+}
+
+awaitGetRandomNumber();
+printCityData("Bloomfield");
